99b1b8a5e4c9313f98a38c3f4a0718c87147cf11
Ladunnitegbe <akinsolaladunni@gmail.com>
2025-12-15 17:39:29 +0100

    Moved page navigation images, worked on page banner styles

--- src/components/PageNavigation/PageBanner.jsx
+++ src/components/PageNavigation/PageBanner.jsx
@@ -1,12 +1,13 @@
-//  ;
-import Clock from "./PageNavigation_images/Clock.svg?react";
-import Location from "./PageNavigation_images/Location.svg?react";
-import Flag from "./PageNavigation_images/Flag.svg?react";
-import ArrowDown from "./PageNavigation_images/ArrowDown.svg?react";
-import Facebook from "./PageNavigation_images/Facebook.svg?react";
-import Twitter from "./PageNavigation_images/Twitter.svg?react";
-import Skype from "./PageNavigation_images/Skype.svg?react";
-import Linkedin from "./PageNavigation_images/Linkedin.svg?react";
+import "./PageNavigation.css";
+
+import Clock from "../../assets/images/PageNavigation_images/Clock.svg?react";
+import Location from "../../assets/images/PageNavigation_images/Location.svg?react";
+import Flag from "../../assets/images/PageNavigation_images/Flag.svg?react";
+import ArrowDown from "../../assets/images/PageNavigation_images/ArrowDown.svg?react";
+import Facebook from "../../assets/images/PageNavigation_images/Facebook.svg?react";
+import Twitter from "../../assets/images/PageNavigation_images/Twitter.svg?react";
+import Skype from "../../assets/images/PageNavigation_images/Skype.svg?react";
+import Linkedin from "../../assets/images/PageNavigation_images/Linkedin.svg?react";
 
 export function PageBanner() {
   return (
@@ -14,14 +15,12 @@ export function PageBanner() {
       <div className="page-banner">
         <section className="page-banner__content">
           <span>
-            {" "}
-            <Clock className="page-banner__content___icon" /> Working : Monday
-            -Friday.9:am - 5:Pm
+            <Clock className="page-banner__content___icon" />
+             Working : Monday-Friday.9:am - 5:Pm
           </span>
           <span>
-            {" "}
-            <Location className="page-banner__content___icon" /> Hudson,
-            Wisconsin(WI), 54016
+            <Location className="page-banner__content___icon" /> 
+            Hudson,Wisconsin(WI), 54016
           </span>
         </section>
 
@@ -32,10 +31,10 @@ export function PageBanner() {
             <ArrowDown className="page-banner___changelang_icon" />
           </div>
           <div className="page-banner___icons">
-            <Facebook className="page-banner__icon" />
-            <Twitter className="page-banner__icon" />
-            <Skype className="page-banner__icon" />
-            <Linkedin className="page-banner__icon" />
+            <Facebook className="page-banner___icons_icon" />
+            <Twitter className="page-banner___icons_icon" />
+            <Skype className="page-banner___icons_icon" />
+            <Linkedin className="page-banner___icons_icon" />
           </div>
         </section>
       </div>
