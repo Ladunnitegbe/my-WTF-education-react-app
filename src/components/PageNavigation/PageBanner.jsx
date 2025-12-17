1f929d5ca3ab95bd9172d90869904c2fdffecfa9
Ladunnitegbe <akinsolaladunni@gmail.com>
2025-12-17 18:10:52 +0100

    change logo directory

--- src/components/PageNavigation/PageBanner.jsx
+++ src/components/PageNavigation/PageBanner.jsx
@@ -28,7 +28,7 @@ export function PageBanner() {
           <div className="page-banner___icons-innerwrapper">
           <div className="page-banner___changelang">
             {/* <Flag className="page-banner___changelang_icon" /> */}
-            <img src="src/assets/images/PageNavigation_images/Flag.png" alt="Flag" className="page-banner___changelang_icon flag" />
+            <img src="../src/assets/images/PageNavigation_images/Flag.png" alt="Flag" className="page-banner___changelang_icon flag" />
             English
             <ArrowDown className="page-banner___changelang_icon arrowdown" />
           </div>
